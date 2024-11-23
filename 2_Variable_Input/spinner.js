d15f0daa47d87ec270e25adf1e22e03bb7a4b271
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-11-23 15:31:16 -0500

    Added dynamic text to the title and the output

--- 2_Variable_Input/spinner.js
+++ 2_Variable_Input/spinner.js
@@ -1,5 +1,7 @@
 let wheel = document.querySelector(".wheel");
-let wheel1 = document.querySelector(".wheel1");
+let title = document.querySelector(".title");
+let output = document.querySelector(".output");
+let result = document.querySelector(".result");
 let spinBtn = document.querySelector(".spinBtn");
 let value = 0;
 
@@ -15,6 +17,10 @@ let objectplacholder = {
   output: "Here is the placeholder output tag",
 };
 
+
+/* This will change based off what object we are inputting */
+let selectionObject = objectplacholder;
+
 let colorArray = [
   "#db7093",
   "#20b2aa",
@@ -26,13 +32,23 @@ let colorArray = [
   "#4169e1",
 ];
 
-createWheel(objectplacholder);
+
+createWheel(selectionObject);
+spinBtn.addEventListener("click", () => {
+  spinTheWheel();
+});
+
 
 function createWheel(object) {
+title.innerHTML = `<p>${object.title}</p>`
+output.innerHTML = object.output
+let result = document.createElement("p");
+output.appendChild(result);
+result.className = "result";
   for (let i = 0; i < object.array.length; i++) {
     let wedge = document.createElement("div");
     let colorChoice = colorArray[i%8];
-    wedge.className = "number";
+    wedge.className = "wedge";
     wedge.style = `transform: rotate(calc((360/${object.array.length})*${i}deg)); clip-path: polygon(
     0 0,
     ${(360/object.array.length)+((360-object.array.length)*.025)}% 0,
@@ -52,8 +68,15 @@ function spinTheWheel() {
   value += Math.ceil(Math.random() * 3600);
   console.log("New Value", value);
   wheel.style.transform = `rotate(${value}deg)`;
+  resultReveal(value);
 }
 
-spinBtn.addEventListener("click", () => {
-  spinTheWheel();
-});
+function resultReveal(rotation){
+  console.log("Result Rotation", rotation);
+  let finalResult = rotation%360;
+  console.log("Final Result", finalResult);
+  //TODO I need code in here to output the correct result
+  result.innerHTML= selectionObject.array[(360/selectionObject.array.length)]
+
+}
+
