96f51caf68daad09d27262cdbaeaa43f11cecbed
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-11-27 19:41:19 -0500

    Added Cap'n West's special shots

---
+++ Spinner-Data/West.js
@@ -0,0 +1,17 @@
+const westBullets = {
+  title: "Cap'n West's Bullets",
+  array: [
+    "YEET",
+    "Hinder",
+    "YEET",
+    "Normal",
+    "Exhaustion",
+    "YEET",
+    "Exhaustion",
+    "YEET",
+    "Normal",
+  ],
+  output: "Cap'n West has used the following Bullet!",
+};
+
+export default westBullets;
