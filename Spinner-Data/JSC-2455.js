ca8073aeab57d49ac7bcb37369db87b2f4900533
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-03 12:40:38 -0500

    End of  meeting

--- Spinner-Data/JSC-2455.js
+++ Spinner-Data/JSC-2455.js
@@ -1,6 +1,6 @@
 const jsc2455 = {
   title: "Job Search Council #2455",
-  array: ["Byron C.", "Courtney L.", "John I.",/*  "Joseph M.", */ "Varun G."],
+  array: ["Byron C.", "Courtney L.", "John I.", "Joseph M.", "Varun G."],
   output: "Who's turn is it to talk",
 };
 
